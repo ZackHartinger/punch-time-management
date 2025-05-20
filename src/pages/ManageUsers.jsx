8063fcb2bfa522e8c850543c563ec3c452d89eee
ZackHartinger <zackhrtngr@gmail.com>
2025-05-19 20:12:32 -0700

    ManageUsers page is fetching users from the backend.

--- src/pages/ManageUsers.jsx
+++ src/pages/ManageUsers.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import PageTitle from '../components/PageTitle'
+import { ToastContainer, toast } from 'react-toastify'
 
 const ManageUsers = () => {
-    const [title, setTitle] = useState("Manage Users")
+    const [title, setTitle] = useState("Manage Users");
+    const [users, setUsers] = useState();
+    const baseUrl = import.meta.env.VITE_PUNCH_API_BASE_URL;
+
+    useEffect(() => {
+        const getUsers = async () => {
+            try {
+                const response = await fetch(baseUrl + 'AppUsers', {
+                    credentials: 'include'
+                })
+                const json = await response.json();
+                if (response.ok) {
+                    setUsers(json);
+                }
+            }
+            catch (error) {
+                toast.error('An error occured handling your reequest')
+            }
+        }
+
+        getUsers();
+    }, [])
+
     return (
         <div className="container pt-3">
             <PageTitle title={title}></PageTitle>
+            <ToastContainer></ToastContainer>
         </div>
     )
 }
